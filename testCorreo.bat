66c8eb914a7ec6fcfeb3dd4942f2ad9bd0114f84
Saycoron <anventas4ml@gmail.com>
2018-06-02 09:35:24 +0200

    Modificacion funciones y test
    Verificacion de rutas todas las posibilidades, test OK
    Refactor de funciones
    
     Changes to be committed:
    	new file:   .gitignore
    	modified:   testCorreo.bat

--- testCorreo.bat
+++ testCorreo.bat
@@ -1,20 +1,17 @@
 @echo off
-
 :: =========================================================================
 ::                   Author: lola-rica                                     =
 ::                   lolatmp@gmail.com                                     =
 ::                    github/Saycoron                                      =
 :: =========================================================================
-
 title /!\ Blat Mail /!\ By:Lola 
-
 ::CFG +++++++++++++++++++++++++++++++++++++++++++++++++++
 ::RUTA CARPETA PRINCIPAL
 set RUTAPRINCIPAL=C:\MAIL
 ::+++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ::RUTA DE FICHEROS
 set RUTALOG=%RUTAPRINCIPAL%\LOG.txt
-set RUTATESTIGO=%RUTAPRINCIPAL%\TESTIGO.txt
+set RUTATESTIGOCONEXION=%RUTAPRINCIPAL%\TESTIGOCONEXION.txt
 ::CORREO
 set DE=test.com
 set PARA="test.com"
@@ -24,45 +21,49 @@ set MENSAJE="Se inicio el equipo a las %date% %time%"
 
 set SUJETOADJ="Adjunto inicio equipo"
 set MENSAJEADJ="Adjunto inicio equipo"
-
 ::CUENTA
 set SERVER="smtp.com"
 set USUARIO="test.com"
 SET PASSWORD="test"
 ::+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+goto VERIFICARCONEXION
 
 :VERIFICARCONEXION
-if exist %RUTATESTIGO% del %RUTATESTIGO%
+if exist %RUTATESTIGOCONEXION% del %RUTATESTIGOCONEXION%
 set ERRORLEVEL =
-ping -n 1 www.google.com > %RUTATESTIGO%
-@find /C "La solicitud de ping no pudo encontrar el host" %RUTATESTIGO% > null
-if %ERRORLEVEL%==0 call:VERIFICASIEXISTELOG "NO" else call:VERIFICASIEXISTELOG "SI"
+ping -n 1 www.google.com > %RUTATESTIGOCONEXION%
+@find /C "La solicitud de ping no pudo encontrar el host" %RUTATESTIGOCONEXION% > null
+if %ERRORLEVEL%==0 call :VERIFICASIEXISTELOG "NO"
+if %ERRORLEVEL%==1 call :VERIFICASIEXISTELOG "SI"
 
 :VERIFICASIEXISTELOG
 set EXISTECONNECT=%~1
-
-if %EXISTECONNECT% EQU "SI" (
-	if exist %RUTALOG% ( call:AGREGALINEAENLOG "SI") else ( goto ENVIARCORREO )
+if %EXISTECONNECT% EQU SI (
+	if exist %RUTALOG% ( call :AGREGALINEAENLOG "SI" ) else ( call :ENVIARCORREO )
 ) else ( call:AGREGALINEAENLOG "NO" )
+goto:EOF
 
 :AGREGALINEAENLOG
 set ENVIALOG=%~1
 if not exist %RUTALOG% echo Creacion de fichero  ----------------------------------------------------------- >>%RUTALOG%
 echo Se inicio el equipo a las %date% %time% >> %RUTALOG%
+if %ENVIALOG% EQU SI ( call :ENVIARCORREOADJ ) else ( goto FIN )
+goto:EOF
 
-if %ENVIALOG% EQU "SI" ( goto ENVIARCORREOADJ ) else ( goto FIN )
+:ENVIARCORREO
+blat -body %MENSAJE% -to %PARA% -subject %SUJETO% -i %DE% -server %SERVER% -f %DE% -u %USUARIO% -pw %PASSWORD% -noh
+if %ERRORLEVEL%==0 ( goto FIN ) else ( call :AGREGALINEAENLOG "NO" )
+goto:EOF
+
+:ENVIARCORREOADJ
+blat -body %MENSAJEADJ% -to %PARA% -subject %SUJETOADJ% -i %DE% -server %SERVER% -f %DE% -u %USUARIO% -pw %PASSWORD% -attach %RUTALOG% -noh
+if %ERRORLEVEL%==0 ( call :ELIMINALOG ) else ( goto FIN )
+goto:EOF
 
 :ELIMINALOG
 DEL %RUTALOG%
 goto FIN
-
-:ENVIARCORREO
-blat -body %MENSAJE% -to %PARA% -subject %SUJETO% -i %DE% -server %SERVER% -f %DE% -u %USUARIO% -pw %PASSWORD% -noh
-if %ERRORLEVEL%==0 goto FIN else call:AGREGALINEAENLOG "NO"
-
-:ENVIARCORREOADJ
-blat -body %MENSAJEADJ% -to %PARA% -subject %SUJETOADJ% -i %DE% -server %SERVER% -f %DE% -u %USUARIO% -pw %PASSWORD% -attach %RUTALOG% -noh
-if %ERRORLEVEL%==0 goto ELIMINALOG else goto FIN
+goto:EOF
 
 :FIN
 exit
